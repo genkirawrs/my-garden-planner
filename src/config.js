1e27fdc9e0124265aecb9322e3dda6e683af9cdc
Rosalind H <rosalind.hung99@gmail.com>
2020-09-07 09:56:43 -0700

    for heroku connection

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 export default {
-    API_ENDPOINT: 'http://localhost:8000/api',
+    API_ENDPOINT: 'https://damp-fjord-22014.herokuapp.com/api',
+//    API_ENDPOINT: 'http://localhost:8000/api',
     ACCUWEATHER_API_KEY: '5Mkj5meubfrWYTTaz1hdGcs5odpLCGpp',
     OWM_API_KEY: 'a528ca16a527c9e31662ff3278edd653',
 }
